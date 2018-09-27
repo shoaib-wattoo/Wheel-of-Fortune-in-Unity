eb01622c6f2c39920112541aac67b6d7a8bce74d
DESKTOP-KF35RC4\ASUS <shoaib.wattoo@gmail.com>
2018-09-27 15:44:04 +0500

    first commit

---
+++ Assets/Scripts/SpinWheel.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpinWheel : MonoBehaviour
+{
+	public List<int> prize;
+	public List<AnimationCurve> animationCurves;
+	
+	private bool spinning;	
+	private float anglePerItem;	
+	private int randomTime;
+	private int itemNumber;
+	
+	void Start(){
+		spinning = false;
+		anglePerItem = 360/prize.Count;		
+	}
+	
+	void  Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && !spinning) {
+		
+			randomTime = Random.Range (1, 4);
+			itemNumber = Random.Range (0, prize.Count);
+			float maxAngle = 360 * randomTime + (itemNumber * anglePerItem);
+			
+			StartCoroutine (SpinTheWheel (5 * randomTime, maxAngle));
+		}
+	}
+	
+	IEnumerator SpinTheWheel (float time, float maxAngle)
+	{
+		spinning = true;
+		
+		float timer = 0.0f;		
+		float startAngle = transform.eulerAngles.z;		
+		maxAngle = maxAngle - startAngle;
+		
+		int animationCurveNumber = Random.Range (0, animationCurves.Count);
+		Debug.Log ("Animation Curve No. : " + animationCurveNumber);
+		
+		while (timer < time) {
+		//to calculate rotation
+			float angle = maxAngle * animationCurves [animationCurveNumber].Evaluate (timer / time) ;
+			transform.eulerAngles = new Vector3 (0.0f, 0.0f, angle + startAngle);
+			timer += Time.deltaTime;
+			yield return 0;
+		}
+		
+		transform.eulerAngles = new Vector3 (0.0f, 0.0f, maxAngle + startAngle);
+		spinning = false;
+			
+		Debug.Log ("Prize: " + prize [itemNumber]);//use prize[itemNumnber] as per requirement
+	}	
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# how-to-make-a-wheel-of-fortune-in-unity-the-easiest-way
+Learn How to Make a Wheel of Fortune in Unity the Easiest Way. Step By Step Guide to Make Wheel of Fortune in Unity with Demo. Wheel of Fortune in Unity Example.
+You can find complete tutorial on [How to Make a Wheel of Fortune in Unity the Easiest Way](http://www.theappguruz.com/blog/how-to-make-a-wheel-of-fortune-in-unity-the-easiest-way).
+
+This Tutorial has been presented by The App Guruz - One of the best [Mobile Game Development Company in India](http://www.theappguruz.com/3d-game-development/).
